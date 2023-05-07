5daba3e5507c9b2d20af8ba9d0fad2ecc7da0561
modestpharaoh <30378999+modestpharaoh@users.noreply.github.com>
2023-05-07 12:40:53 +0100

    Update the code to match the original component
    Update the code to match the original component

--- islamic_prayer_times_ie/const.py
+++ islamic_prayer_times_ie/const.py
@@ -1,24 +1,20 @@
 """Constants for the Islamic Prayer component."""
-DOMAIN = "islamic_prayer_times_ie"
-NAME = "Islamic Prayer Times - IE"
+from typing import Final
+
+from prayer_times_calculator import PrayerTimesCalculator
+
+DOMAIN: Final = "islamic_prayer_times_ie"
+NAME: Final = "Islamic Prayer Times - IE"
 PRAYER_TIMES_ICON = "mdi:calendar-clock"
 
-SENSOR_TYPES = {
-    "Fajr": "prayer",
-    "Sunrise": "time",
-    "Dhuhr": "prayer",
-    "Asr": "prayer",
-    "Maghrib": "prayer",
-    "Isha": "prayer",
-    "Midnight": "time",
-}
+CONF_CALC_METHOD: Final = "calculation_method"
 
-CONF_CALC_METHOD = "calculation_method"
 
 #CALC_METHODS = ["isna", "karachi", "mwl", "makkah", "icci"]
+#CALC_METHODS: list[str] = list(PrayerTimesCalculator.CALCULATION_METHODS)
 
 CALC_METHODS = ["jafari", "karachi", "isna", "mwl", "makkah", "egypt", "tehran", "gulf", "kuwait", "qatar", "singapore", "france", "turkey", "russia", "icci"]
 
-DEFAULT_CALC_METHOD = "icci"
+DEFAULT_CALC_METHOD: Final = "icci"
 
 DATA_UPDATED = "Islamic_prayer_data_updated"
