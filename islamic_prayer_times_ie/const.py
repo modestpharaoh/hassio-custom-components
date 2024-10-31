ec1a4f265ed65ed55a5d10afd4f23ae9d718d042
modestpharaoh <modestpharaoh@gmail.com>
2024-10-31 08:39:55 +0000

    set ie-hicc as default

--- islamic_prayer_times_ie/const.py
+++ islamic_prayer_times_ie/const.py
@@ -11,6 +11,6 @@ CONF_CALC_METHOD: Final = "calculation_method"
 
 CALC_METHODS = ["jafari", "karachi", "isna", "mwl", "makkah", "egypt", "tehran", "gulf", "kuwait", "qatar", "singapore", "france", "turkey", "russia", "ie-icci", "ie-mcnd", "ie-hicc"]
 
-DEFAULT_CALC_METHOD: Final = "ie-mcnd"
+DEFAULT_CALC_METHOD: Final = "ie-hicc"
 
 DATA_UPDATED = "Islamic_prayer_data_updated"
